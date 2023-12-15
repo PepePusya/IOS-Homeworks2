0130325663cde088c2838bdf0e7c086259d652de
Anna Zaytseva <info@annykate-models.com>
2023-12-15 13:12:34 +0900

    Initial Commit

---
+++ NavigationLast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavigationLast
+//
+//  Created by Anna Zaytseva on 2023/12/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
