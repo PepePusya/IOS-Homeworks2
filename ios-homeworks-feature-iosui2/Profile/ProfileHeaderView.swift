1891c1d7df98fe64bfc638c522ddf1ce29e02998
Anna Zaytseva <info@annykate-models.com>
2024-01-02 23:54:13 +0900

    Remove changes3

--- ios-homeworks-feature-iosui2/Profile/ProfileHeaderView.swift
+++ ios-homeworks-feature-iosui2/Profile/ProfileHeaderView.swift
@@ -1,15 +1,12 @@
 
-
-import Foundation
 import UIKit
-
 class ProfileHeaderView: UIView {
     
     let avatarImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.image = UIImage(named: "avatarImage")
-        imageView.layer.cornerRadius = 45.0
+        imageView.layer.cornerRadius = 50.0
         imageView.layer.borderWidth = 3.0
         imageView.layer.borderColor = UIColor.white.cgColor
         imageView.layer.masksToBounds = true
@@ -19,7 +16,7 @@ class ProfileHeaderView: UIView {
     let setStatusButton: UIButton = {
         let button = UIButton(type: .system)
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Show status", for: .normal)
+        button.setTitle("Set status", for: .normal)
         button.setTitleColor(.white, for: .normal)
         button.backgroundColor = .systemBlue
         button.layer.cornerRadius = 4.0
@@ -52,13 +49,19 @@ class ProfileHeaderView: UIView {
     let statusTextField: UITextField = {
         let textField = UITextField()
         textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.backgroundColor = .white
         textField.placeholder = "Input Text"
-        textField.borderStyle = .roundedRect
-        textField.backgroundColor = UIColor(white: 0.9, alpha: 1.0)
-        textField.font = UIFont.systemFont(ofSize: 14)
-        textField.textColor = .systemBrown
+        textField.font = UIFont.systemFont(ofSize: 15.0)
+        textField.textColor = .black
+        textField.layer.borderWidth = 1.0
+        textField.layer.borderColor = UIColor.black.cgColor
+        textField.layer.cornerRadius = 12.0
         textField.textAlignment = .left
         textField.contentVerticalAlignment = .center
+        
+        let leftPaddingView = UIView(frame:CGRect(x: 0, y: 0, width: 8.0, height: textField.frame.height))
+        textField.leftView = leftPaddingView
+        textField.leftViewMode = .always
         return textField
         
         
@@ -86,24 +89,29 @@ class ProfileHeaderView: UIView {
         NSLayoutConstraint.activate([
             avatarImageView.topAnchor.constraint(equalTo: safeAreaGuide.topAnchor, constant: 16.0),
             avatarImageView.leadingAnchor.constraint(equalTo:safeAreaGuide.leadingAnchor, constant: 16.0),
-            avatarImageView.widthAnchor.constraint(equalToConstant: 90.0),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 100.0),
             avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor),
             
-            setStatusButton.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 16.0),
+            
+            fullNameLabel.topAnchor.constraint(equalTo: avatarImageView.topAnchor),
+            
+            fullNameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 8.0),
+            
+            
+            statusLabel.topAnchor.constraint(equalTo: fullNameLabel.bottomAnchor, constant: 4.0),
+            statusLabel.leadingAnchor.constraint(equalTo: fullNameLabel.leadingAnchor),
+            
+            
+            setStatusButton.topAnchor.constraint(equalTo: statusTextField.bottomAnchor, constant:  12.0),
             setStatusButton.leadingAnchor.constraint(equalTo: safeAreaGuide.leadingAnchor, constant: 16.0),
             setStatusButton.trailingAnchor.constraint(equalTo: safeAreaGuide.trailingAnchor, constant: -16.0),
             setStatusButton.heightAnchor.constraint(equalToConstant: 50.0),
             
-            fullNameLabel.topAnchor.constraint(equalTo: safeAreaGuide.topAnchor, constant: 27.0),
-            fullNameLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            statusTextField.topAnchor.constraint(equalTo: statusLabel.bottomAnchor, constant: 8.0),
             
-            statusLabel.topAnchor.constraint(equalTo: setStatusButton.bottomAnchor, constant: -84.0),
-            statusLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
-            
-            statusTextField.topAnchor.constraint(equalTo: setStatusButton.bottomAnchor, constant: 8.0),
-            statusTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12.0),
-            statusTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12.0),
-            statusTextField.heightAnchor.constraint(equalToConstant: 30.0)
+            statusTextField.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 8.0),
+            statusTextField.trailingAnchor.constraint(equalTo: safeAreaGuide.trailingAnchor, constant: -16.0),
+            statusTextField.heightAnchor.constraint(equalToConstant: 40.0)
         ])
         setStatusButton.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
     }
@@ -111,5 +119,3 @@ class ProfileHeaderView: UIView {
         print(statusLabel.text ?? "")
     }
 }
-
-
