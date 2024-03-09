7ce384d0abbe0db00e4a38f16e0ea1fbf8b1c12c
Anna Zaytseva <info@annykate-models.com>
2024-03-09 19:29:17 +0900

    Photo collection View cell

--- ios-homeworks-feature-iosui2/Profile/ProfileViewController.swift
+++ ios-homeworks-feature-iosui2/Profile/ProfileViewController.swift
@@ -5,6 +5,8 @@ class ProfileViewController: UIViewController {
     private var tableView: UITableView!
     private var posts: [Post] = []
 
+    private let photosCellIdentifier = "PhotosTableViewCell"
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableView()
@@ -12,18 +14,19 @@ class ProfileViewController: UIViewController {
     }
 
     private func setupTableView() {
-        tableView = UITableView()
+        tableView = UITableView(frame: .zero, style: .grouped)
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(PostTableViewCell.self, forCellReuseIdentifier: "PostTableViewCell")
+        tableView.register(PhotosTableViewCell.self, forCellReuseIdentifier: photosCellIdentifier)
         
         view.addSubview(tableView)
         tableView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            tableView.rightAnchor.constraint(equalTo: view.rightAnchor)
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
         ])
         
         tableView.estimatedRowHeight = 400.0
@@ -33,29 +36,40 @@ class ProfileViewController: UIViewController {
         headerView.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: 220.0)
         tableView.tableHeaderView = headerView
     }
-
+    
     private func loadDummyData() {
-        posts = [
-            Post(author: "Pepe Cat", description: "She is a Liar", imageName: "liarCat", likes: 120, views: 300),
-            Post(author: "Crazy Dog", description: "Miting with Pepe Cat", imageName: "postImage1", likes: 100, views: 230),
-            Post(author: "Crazy Dog", description: "Are you ok, my friend? Are you became a vegetarian?", imageName: "veganCat", likes: 29, views: 450),
-            Post(author: "Pusya Rabbit", description: "Haha, soon i will take over the world!", imageName: "Pusya", likes: 400, views: 402)
-        ]
+        posts = Post.getDummyData()
     }
 }
 
 extension ProfileViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+       
+        return 2
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return posts.count
+    
+        return section == 0 ? 1 : posts.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "PostTableViewCell", for: indexPath) as? PostTableViewCell else {
-            fatalError("Cell not found")
+        if indexPath.section == 0 {
+            
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: photosCellIdentifier, for: indexPath) as? PhotosTableViewCell else {
+                fatalError("PhotosTableViewCell not found")
+            }
+            
+            return cell
+        } else {
+         
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "PostTableViewCell", for: indexPath) as? PostTableViewCell else {
+                fatalError("PostTableViewCell not found")
+            }
+            let post = posts[indexPath.row]
+            cell.configure(with: post)
+            return cell
         }
-        let post = posts[indexPath.row]
-        cell.configure(with: post)
-        return cell
     }
 }
 
@@ -66,5 +80,13 @@ extension ProfileViewController: UITableViewDelegate {
 
     func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
         return 400.0
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 0 {
+            let photosViewController = PhotosViewController()
+            navigationController?.pushViewController(photosViewController, animated: true)
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
--- ios-homeworks-feature-iosui2/ViewController.swift
+++ ios-homeworks-feature-iosui2/ViewController.swift
@@ -4,16 +4,78 @@
 //
 //  Created by Anna Zaytseva on 2023/11/08.
 //
-
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    private var tableView: UITableView!
+    private var photoGallery: [UIImage] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadImages()
+        setupTableView()
     }
 
+    private func loadImages() {
+        for i in 1...20 {
+            if let image = UIImage(named: String(format: "%02d", i)) {
+                photoGallery.append(image)
+            }
+        }
+    }
 
+    private func setupTableView() {
+        tableView = UITableView(frame: .zero, style: .plain)
+        tableView.register(PhotosTableViewCell.self, forCellReuseIdentifier: "PhotosTableViewCell")
+        tableView.dataSource = self
+        tableView.delegate = self
+
+        view.addSubview(tableView)
+
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+      
+        return 2
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+        return section == 0 ? 1 : 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "PhotosTableViewCell", for: indexPath) as? PhotosTableViewCell else {
+                fatalError("PhotosTableViewCell not found")
+            }
+            
+            cell.configure(with: Array(photoGallery.prefix(4)))
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "OtherCell", for: indexPath)
+        
+            cell.textLabel?.text = "Row \(indexPath.row)"
+            return cell
+        }
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+      
+        return indexPath.section == 0 ? 200 : UITableView.automaticDimension
+    }
+
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return indexPath.section == 0 ? 200 : UITableView.automaticDimension
+    }
 }
-
